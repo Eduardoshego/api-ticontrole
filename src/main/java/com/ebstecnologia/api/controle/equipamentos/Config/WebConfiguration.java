40c866f760436d8cc8a1ed778eb59036cbc28a68
Eduardshego <eduarodshego1@gmail.com>
2021-12-04 14:45:40 -0300

    Reconfigurando os cors da aplicação

--- src/main/java/com/ebstecnologia/api/controle/equipamentos/Config/WebConfiguration.java
+++ src/main/java/com/ebstecnologia/api/controle/equipamentos/Config/WebConfiguration.java
@@ -1,30 +1,40 @@
 package com.ebstecnologia.api.controle.equipamentos.Config;
 
 import lombok.val;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class WebConfiguration {
 
     @Bean
-    public CorsConfigurationSource getConfigurationSource() {
-        val source = new UrlBasedCorsConfigurationSource();
-        val configuration = new CorsConfiguration();
-        configuration.addAllowedOrigin("http://localhost:4200");
-        configuration.addAllowedOrigin("https://ti-suporte-front-end.herokuapp.com");
-        val all = Collections.singletonList("*");
-        configuration.setAllowedMethods(all);
-        configuration.setAllowedHeaders(all);
-        configuration.setExposedHeaders(all);
-        configuration.setMaxAge(3600L);
-        configuration.setAllowCredentials(true);
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public FilterRegistrationBean<CorsFilter> corsFilterFilterRegistrationBean(){
+        List<String> all = Arrays.asList("*");
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Arrays.asList("https://ti-suporte-front-end.herokuapp.com"));
+        corsConfiguration.setAllowedHeaders(all);
+        corsConfiguration.setAllowedMethods(all);
+        corsConfiguration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        CorsFilter corsFilter = new CorsFilter(source);
+        FilterRegistrationBean<CorsFilter> filter = new FilterRegistrationBean<>(corsFilter);
+        filter.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
+        return filter;
     }
+
 }
