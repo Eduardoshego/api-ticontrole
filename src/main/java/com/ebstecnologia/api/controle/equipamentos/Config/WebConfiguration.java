2081610d93f6b046951c68cc3157cc5d59b410e6
Eduardshego <eduarodshego1@gmail.com>
2021-12-04 14:27:34 -0300

    Reconfigurando os cors da aplicação

--- src/main/java/com/ebstecnologia/api/controle/equipamentos/Config/WebConfiguration.java
+++ src/main/java/com/ebstecnologia/api/controle/equipamentos/Config/WebConfiguration.java
@@ -1,24 +1,19 @@
 package com.ebstecnologia.api.controle.equipamentos.Config;
 
 import lombok.val;
-import org.springframework.core.Ordered;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 @Configuration
 public class WebConfiguration {
 
-    private CorsConfigurationSource getConfigurationSource() {
+    @Bean
+    public CorsConfigurationSource getConfigurationSource() {
         val source = new UrlBasedCorsConfigurationSource();
         val configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost:4200");
