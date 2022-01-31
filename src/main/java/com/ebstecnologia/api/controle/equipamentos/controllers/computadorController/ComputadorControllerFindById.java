d82ed11e7379afc4e33343993b221a4b9aaaeda9
Eduardshego <eduarodshego1@gmail.com>
2022-01-31 15:44:47 -0300

    Concertando o endpoint ComputadoresFindById

--- src/main/java/com/ebstecnologia/api/controle/equipamentos/controllers/computadorController/ComputadorControllerFindById.java
+++ src/main/java/com/ebstecnologia/api/controle/equipamentos/controllers/computadorController/ComputadorControllerFindById.java
@@ -5,15 +5,17 @@ import com.ebstecnologia.api.controle.equipamentos.services.computadorServices.C
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/computadores")
+@RestController
+@RequestMapping("/api/computadores/{id}")
 @RequiredArgsConstructor
 public class ComputadorControllerFindById {
 
     private final ComputadorServiceFindById service;
 
-    @GetMapping("/{id}")
+    @GetMapping
     public Computador findById(@PathVariable Integer id){
         return service.findById(id);
     }
--- src/main/java/com/ebstecnologia/api/controle/equipamentos/controllers/ipAdrresController/IpAdrresControllerFindById.java
+++ src/main/java/com/ebstecnologia/api/controle/equipamentos/controllers/ipAdrresController/IpAdrresControllerFindById.java
@@ -5,9 +5,11 @@ import com.ebstecnologia.api.controle.equipamentos.services.ipAdrresServices.IpA
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/ips/{id}")
+@RestController
+@RequestMapping("/api/ips/{id}")
 @RequiredArgsConstructor
 public class IpAdrresControllerFindById {
 
